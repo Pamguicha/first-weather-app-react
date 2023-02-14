29bad4e91231b766bb2b0793b2d6b8fdb03c1c1d
Pamguicha <pamelaguicharrousse@gmail.com>
2023-02-15 10:54:50 +1100

    new function and event created to show Fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,13 +2,19 @@ import React, { useState} from "react";
 
 export default function WeatherTemperature(props){
     const [unit, setUnit] = useState("celsius");
+    function showFahrenheit(event){
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
     if (unit === "celsius") {
     return (
 <> 
  <span className="space"> {Math.round(props.celsius)} </span>
           <span className="units">
               °C | 
-              <a href="/">°F 
+              <a href="/"
+              onClick={showFahrenheit}>
+                °F 
               </a>
           </span>
 </>
