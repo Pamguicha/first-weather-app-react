223269162dc1952e9218aa51b0fd2991f66f7bea
Pamguicha <pamelaguicharrousse@gmail.com>
2023-02-15 10:50:11 +1100

    use state created an if statement plus a element

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,22 +1,21 @@
 import React, { useState} from "react";
+
 export default function WeatherTemperature(props){
+    const [unit, setUnit] = useState("celsius");
+    if (unit === "celsius") {
     return (
 <> 
- <span className="space"> {Math.round(props.celsius)} </span>{" "}
+ <span className="space"> {Math.round(props.celsius)} </span>
           <span className="units">
-              °C
-            <span className="line"> | </span>
-            <a
-              id="faherenheit-link"
-              href="/"
-    
-            >
-              °F
-            </a>
+              °C | 
+              <a href="/">°F 
+              </a>
           </span>
 </>
     );
-   
+    } else {
+        return "F";
+    }
 
     } 
 
