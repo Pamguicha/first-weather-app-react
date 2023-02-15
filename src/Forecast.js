90895ce7871dae45c10daf765c8383af2efaa191
Pamguicha <pamelaguicharrousse@gmail.com>
2023-02-16 10:57:55 +1100

    New function and API included for forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,9 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 export default function WeatherForecast(){
+function handleResponse(response){
+    console.log(response.data);
+}
+
+    let apiKey = "de2c40e370d58e257faf07ba4ea95840"; 
+    let longitude = 40.7;
+    let latitude = 74 ;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=0${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
     return (
   
- 
             <div>
       <span> 5-day forecast </span>
       <hr />
