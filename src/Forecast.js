31c688ff412b4eedcf8f8d47e304de4d2f67bed8
Pamguicha <pamelaguicharrousse@gmail.com>
2023-02-20 13:09:58 +1100

    Added use effect and new function

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
 
@@ -8,39 +8,47 @@ export default function WeatherForecast(props){
   let [loaded, setLoaded] = useState (false);
   let [forecast, setForecast] = useState (null);
 
+useEffect(() => {
+  setLoaded(false);
+},[props.coordinates]);
+
 
 function handleResponse(response){
    setForecast(response.data.daily);
    setLoaded(true);
 }
 
-if (loaded) {
-  return (
-     <div className="container">
-    {forecast.map(function (dailyForecast, index) {
-      if (index < 5) {
-      return (
-           <div className="column" key={index}>
-  <ForecastDay data = {dailyForecast}/>
-</div>
-      );
-      }
-    })}
-    
-      </div>
-      
-    
-
-  
-    );
-    } else {
-
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c"; 
+function load (){
+   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c"; 
     let longitude = props.coordinates.lon; 
     let latitude = props.coordinates.lat; 
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
+
+}
+
+if (loaded) {
+  return (
+     <div className="container">
+       {forecast.map(function (dailyForecast, index) {
+      if (index < 5) {
+      return (
+           <div className="column" key={index}>
+            <ForecastDay data = {dailyForecast} />
+          </div>
+      );
+      } else {
+        return  null;
+      }
+    })}
+
+      </div>
+      
+    );
+    } else {
+
+   load();
 
     return null;
     }
